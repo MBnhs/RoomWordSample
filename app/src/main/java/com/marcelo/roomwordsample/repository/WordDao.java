e1d0a9d889af74bd6c96317f4d479d500e5884a4
Marcelo Banhos de Almeida Scavassa <marcelo.almeida@gpnet.com.br>
2018-08-30 16:59:15 -0300

    Primeiro commit

---
+++ app/src/main/java/com/marcelo/roomwordsample/repository/WordDao.java
@@ -0,0 +1,22 @@
+package com.marcelo.roomwordsample.repository;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.Query;
+
+import com.marcelo.roomwordsample.model.Word;
+
+import java.util.List;
+
+@Dao
+public interface WordDao {
+    @Insert
+    void insert(Word word);
+
+    @Query("DELETE FROM word_table")
+    void deleteAll();
+
+    @Query("SELECT * FROM word_table ORDER BY word ASC")
+    LiveData<List<Word>> getAllWords();
+}
